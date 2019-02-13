57b208ec14226067e19dbc5f738e65cb920b3d85
Duncan McGreggor <oubiwann@gmail.com>
2019-02-13 15:35:25 -0600

    Added component system init for CLI usage.

--- src/mm/scylla/graph/components/core.clj
+++ src/mm/scylla/graph/components/core.clj
@@ -24,12 +24,22 @@
 (def janus
   {:janus (component/using
            (janus/create-component)
-           [:config :logging])})
+           [:config :logging :gremlin])})
+
+(def janus-no-logging
+  {:janus (component/using
+           (janus/create-component)
+           [:config :gremlin])})
 
 (def grmln
   {:gremlin (component/using
              (gremlin/create-component)
-             [:config :logging :janus])})
+             [:config :logging])})
+
+(def grmln-no-logging
+  {:gremlin (component/using
+             (gremlin/create-component)
+             [:config])})
 
 (def http-server
   {:httpd (component/using
@@ -53,8 +63,16 @@
     (component/map->SystemMap
       (merge (cfg cfg-data)
              log
-             janus
-             grmln))))
+             grmln
+             janus))))
+
+(defn initialize-with-backend-no-logging
+  []
+  (let [cfg-data (config-lib/data)]
+    (component/map->SystemMap
+      (merge (cfg cfg-data)
+             grmln-no-logging
+             janus-no-logging))))
 
 (defn initialize-with-web
   []
@@ -62,17 +80,20 @@
     (component/map->SystemMap
       (merge (cfg cfg-data)
              log
-             janus
              grmln
+             janus
              http-server))))
 
 (def init-lookup
   {:basic #'initialize-bare-bones
    :backend #'initialize-with-backend
-   :web #'initialize-with-web})
+   :web #'initialize-with-web
+   :cli #'initialize-with-backend-no-logging})
 
 (defn init
   ([]
     (init :web))
   ([mode]
     ((mode init-lookup))))
+
+(def cli #(init :cli))
